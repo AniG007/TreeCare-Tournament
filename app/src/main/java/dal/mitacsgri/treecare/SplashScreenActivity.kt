72e472a16a7feb22df1dd1474fb9b64197366d14
Devansh Maurya <Devansh233@gmail.com>
2019-05-30 14:55:02 -0300

    Added the unity project as a module

--- app/src/main/java/dal/mitacsgri/treecare/SplashScreenActivity.kt
+++ app/src/main/java/dal/mitacsgri/treecare/SplashScreenActivity.kt
@@ -4,17 +4,19 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import gri.unity.treecare.UnityPlayerActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        closeSplashScreen()
     }
 
     private fun closeSplashScreen() {
         Handler().postDelayed( {
-            startActivity(Intent(this@SplashScreenActivity, MenuActivity::class.java))
+            startActivity(Intent(this@SplashScreenActivity, UnityPlayerActivity::class.java))
         }, 4000)
     }
 }

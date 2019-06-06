c138253e9e685286a031323b4bb0b71150d0e0ff
Devansh Maurya <Devansh233@gmail.com>
2019-06-06 16:34:27 -0300

    Added code to read step count. Working but only when Google Fit App is installed.

--- app/src/main/java/dal/mitacsgri/treecare/FitnessAPIActivity.kt
+++ app/src/main/java/dal/mitacsgri/treecare/FitnessAPIActivity.kt
@@ -1,32 +1,40 @@
 package dal.mitacsgri.treecare
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.Scopes
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.common.api.Scope
 import com.google.android.gms.fitness.Fitness
 import com.google.android.gms.fitness.FitnessOptions
+import com.google.android.gms.fitness.FitnessStatusCodes
+import com.google.android.gms.fitness.data.DataPoint
 import com.google.android.gms.fitness.data.DataSet
 import com.google.android.gms.fitness.data.DataType
+import com.google.android.gms.fitness.data.Field
 import com.google.android.gms.fitness.request.DataReadRequest
+import com.google.android.gms.fitness.request.OnDataPointListener
+import com.google.android.gms.fitness.request.SensorRequest
+import kotlinx.android.synthetic.main.activity_fitness_api.*
 import java.text.DateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
 
+class FitnessAPIActivity : AppCompatActivity(),
+    GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+    OnDataPointListener {
 
-class FitnessAPIActivity : AppCompatActivity() {
+    private var mGoogleApiClient: GoogleApiClient? = null
 
     companion object {
         const val GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 4
         const val LOG_TAG = "fitnessapilog"
     }
-
-    lateinit var client: GoogleApiClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,18 +52,61 @@ class FitnessAPIActivity : AppCompatActivity() {
                 GoogleSignIn.getLastSignedInAccount(this),
                 fitnessOptions)
         } else {
-//            accessGoogleFit()
-            displayStepDataForToday()
+            accessGoogleFit()
         }
+
+        // Create a Google Fit Client instance with default user account.
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+            .addApi(Fitness.SENSORS_API)  // Required for SensorsApi calls
+            .addApi(Fitness.HISTORY_API)
+            .useDefaultAccount()
+            .addScope(Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
+            .addScope(Scope(Scopes.FITNESS_BODY_READ_WRITE))
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .build()
+
+        mGoogleApiClient?.connect()
+        //displayStepDataForToday()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
-                accessGoogleFit()
-                displayStepDataForToday()
-            }
+//        if (resultCode == Activity.RESULT_OK) {
+//            if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
+//                accessGoogleFit()
+//                displayStepDataForToday()
+//            }
+//        }
+        if (requestCode == 1001 && resultCode == RESULT_OK) {
+            mGoogleApiClient?.connect()
         }
+    }
+
+    override fun onConnected(connectionHint: Bundle?) {
+        // Connected to Google Fit Client.
+        Fitness.SensorsApi.add(
+            mGoogleApiClient,
+            SensorRequest.Builder()
+                .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                .build(),
+            this
+        )
+        displayStepDataForToday()
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onConnectionFailed(result: ConnectionResult) {
+        // Error while connecting. Try to resolve using the pending intent returned.
+        if (result.errorCode == FitnessStatusCodes.NEEDS_OAUTH_PERMISSIONS) {
+
+                result.startResolutionForResult(this, 1001)
+        }    }
+
+    override fun onDataPoint(p0: DataPoint?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     private fun accessGoogleFit() {
@@ -63,7 +114,7 @@ class FitnessAPIActivity : AppCompatActivity() {
         cal.time = Date()
         val endTime = cal.timeInMillis
         cal.add(Calendar.DAY_OF_MONTH, -1)
-        val startTime = cal.timeInMillis - 10000000000
+        val startTime = cal.timeInMillis - TimeUnit.DAYS.toMillis(1)
 
         val readRequest = DataReadRequest.Builder()
             .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
@@ -71,24 +122,36 @@ class FitnessAPIActivity : AppCompatActivity() {
             .bucketByTime(1, TimeUnit.DAYS)
             .build()
 
+        var steps = 0
+
         Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this)!!)
             .readData(readRequest)
-            .addOnSuccessListener { Log.d(LOG_TAG, "${it.buckets}") }
+            .addOnSuccessListener {
+                Log.d(LOG_TAG, "${it.buckets.size}")
+                it.buckets.forEach {
+                    it.dataSets.forEach {
+                        it.dataPoints.forEach {
+                            steps += it.getValue(Field.FIELD_STEPS).asInt()
+                            Log.d("steps: ", it.getValue(Field.FIELD_STEPS).asInt().toString())
+                        }
+                    }
+                }
+                tvStepsCount.text = steps.toString()
+            }
             .addOnFailureListener { e -> Log.e(LOG_TAG, "onFailure()", e) }
             .addOnCompleteListener { Log.d(LOG_TAG, "onComplete()") }
+
     }
 
     private fun displayStepDataForToday() {
 
-        client = GoogleApiClient.Builder(this)
-            .addApi(Fitness.SENSORS_API)
-            .addScope(Scope(Scopes.FITNESS_BODY_READ_WRITE))
-            .addScope(Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
-            .build()
+        thread {
+            val result = Fitness.HistoryApi.readDailyTotal(mGoogleApiClient, DataType.TYPE_STEP_COUNT_DELTA)
+                .setResultCallback {
+                    showDataSet(it.total!!)
+                }
 
-        val result = Fitness.HistoryApi.readDailyTotal(client, DataType.TYPE_STEP_COUNT_DELTA)
-            .await(1, TimeUnit.MINUTES)
-        showDataSet(result.total!!)
+        }
     }
 
     private fun showDataSet(dataSet: DataSet) {
---
+++ app/src/main/java/dal/mitacsgri/treecare/SensorApiActivity.kt
@@ -0,0 +1,12 @@
+package dal.mitacsgri.treecare
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SensorApiActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sensor_api)
+    }
+}

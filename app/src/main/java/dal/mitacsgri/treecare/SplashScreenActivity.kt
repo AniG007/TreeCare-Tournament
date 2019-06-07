77315001801091cb5adccf0540c2b1b74ef2b3a9
Devansh Maurya <Devansh233@gmail.com>
2019-06-07 13:58:27 -0300

    Layout for login screen done

--- app/src/main/java/dal/mitacsgri/treecare/SplashScreenActivity.kt
+++ app/src/main/java/dal/mitacsgri/treecare/SplashScreenActivity.kt
@@ -11,7 +11,7 @@ class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
 
         val sharedPref = SharedPreferencesProvider(this)
         sharedPref.storeDailyStepCount(6000)
--- app/src/main/java/dal/mitacsgri/treecare/screens/login/LoginActivity.kt
+++ app/src/main/java/dal/mitacsgri/treecare/screens/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package dal.mitacsgri.treecare.screens.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import dal.mitacsgri.treecare.R
 
 class LoginActivity : AppCompatActivity() {
@@ -9,5 +10,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.gray)
     }
 }

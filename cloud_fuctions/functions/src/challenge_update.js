90552c926da614710f087c1e8a06941d37f02d6e
Devansh Maurya <Devansh233@gmail.com>
2019-07-05 15:42:06 -0300

    Update cloud function code to check the end date of challenge and set active flag accordingly

--- cloud_fuctions/functions/src/challenge_update.js
+++ cloud_fuctions/functions/src/challenge_update.js
@@ -1,3 +1,14 @@
+//Update path variables
+process.env.GCLOUD_PROJECT = 'smooth-pivot-242815'
+process.env.FIREBASE_CONFIG = {
+	databaseURL: 'https://smooth-pivot-242815.firebaseio.com',
+	storageBucket: 'smooth-pivot-242815.appspot.com',
+	projectId: 'projectId'
+}
+
+//Function code starts
+
+const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 
 admin.initializeApp({
@@ -9,8 +20,31 @@ admin.initializeApp({
 
 const db = admin.firestore()
 
-db.collection('challenges').get().then((querySnapshot) => {
-	querySnapshot.forEach((doc) => {
-		console.log(`${doc.id} => ${doc.data()}`);
+updateChallengeDaily = functions.https.onRequest((req, res) => {
+
+	db.collection('challenges').get().then((querySnapshot) => {
+		querySnapshot.forEach((doc) => {
+			console.log(`${doc.id} => ${doc.data()}`);
+
+			const challenge = doc.data();
+
+			for (let property in challenge) {
+				if (challenge.hasOwnProperty(property)) {
+					console.log(challenge[property]);
+				}
+			}
+
+			const endTime = challenge.finishTimestamp.toDate().getTime();
+			const currentTime = new Date().getTime()
+			
+			if (endTime < currentTime ) {
+				db.collection('challenges').doc(challenge.name)
+					.set({
+						active: false
+					}, {merge: true})
+			}
+		})
 	})
 })
+
+updateChallengeDaily()

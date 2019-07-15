bafd4f4c96b05d935f06b0d09b35da221ba51d2e
Devansh Maurya <Devansh233@gmail.com>
2019-07-15 10:35:55 -0300

    Fixed a bug in loading images in Tournament mode home by loading images using Glide

--- app/src/main/java/dal/mitacsgri/treecare/screens/tournamentmode/TournamentModeFragment.kt
+++ app/src/main/java/dal/mitacsgri/treecare/screens/tournamentmode/TournamentModeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.bumptech.glide.Glide
 import dal.mitacsgri.treecare.R
 import kotlinx.android.synthetic.main.fragment_tournament_mode.view.*
 
@@ -21,6 +22,9 @@ class TournamentModeFragment : Fragment() {
             tournaments.setOnClickListener {
                 findNavController().navigate(R.id.action_tournamentModeFragment_to_tournamentsFragment)
             }
+
+            Glide.with(this).load(R.drawable.tournament_stadium).into(tournamentsImage)
+            Glide.with(this).load(R.drawable.team_run).into(teamsImage)
         }
 
         return view
